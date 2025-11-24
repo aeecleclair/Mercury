5bdb7daf50889f3fbc4ce69521552efb57d773dd
Marc-Andrieu <marc.andrieu@outlook.com>
2025-11-25 00:01:29 +0100

    New ping command

---
+++ src/commands/utils/Ping.ts
@@ -0,0 +1,19 @@
+import { ApplicationCommandType, ChatInputCommandInteraction } from "discord.js";
+import Command from "../../utils/Command";
+import { BaseContext } from "../../utils/Context";
+
+export default class Ping extends Command {
+	constructor() {
+		super({
+			type: ApplicationCommandType.ChatInput,
+			name: "ping",
+			category: "utils",
+			description: "Test the latency of the bot to Discord.",
+			examples: ["ping"]
+		});
+	}
+
+	async run(ctx: BaseContext<ChatInputCommandInteraction>) {
+		ctx.reply(`${ctx.client.ws.ping} ms`);
+	}
+}
