c8185e6eed256e9642afa66b7476b36c2eb04ed0
Warix <39554785+warix8@users.noreply.github.com>
2026-09-03 21:05:06 +0200

    message content intent

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 "use strict";
 
 // On récupère des classes ici
-import { Client, GatewayIntentBits, IntentsBitField, Partials } from "discord.js";
+import { Client, IntentsBitField, Partials } from "discord.js";
 import CommandsManager from "./src/utils/CommandsManager";
 import EventsManager from "./src/utils/EventsManager.js";
 import Logger from "./src/utils/Logger";
@@ -34,11 +34,7 @@ class Bot extends Client {
 				IntentsBitField.Flags.DirectMessageTyping,
 				IntentsBitField.Flags.GuildScheduledEvents,
 				IntentsBitField.Flags.GuildMessageTyping,
-				IntentsBitField.Flags.GuildMessages,
-				GatewayIntentBits.Guilds,
-				GatewayIntentBits.GuildMessages,
-				GatewayIntentBits.MessageContent,
-				GatewayIntentBits.GuildMembers
+				IntentsBitField.Flags.MessageContent,
 			]
 		});
 		this.config = config; // récupérer la config
